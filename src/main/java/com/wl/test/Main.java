c2d5121103222133860ea0c1bbef8a54e8043b46
wcl2184 <wcl2184@163.com>
2018-03-29 11:14:57 +0800

    xxxxxx的

---
+++ src/main/java/com/wl/test/Main.java
@@ -0,0 +1,44 @@
+package com.wl.test;
+
+public class Main {
+
+	public static void main(String[] args)  throws Exception{
+		Object lock = new Object();
+		
+		
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				try {
+					Thread.sleep(4000);
+					synchronized (lock) {
+					
+					lock.notify();
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		},"t1");
+	    t1.start();
+		
+		
+		synchronized (lock) {
+			
+			try {
+				lock.wait();
+				System.out.println("唤醒");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+		
+	}
+	
+	
+
+}
